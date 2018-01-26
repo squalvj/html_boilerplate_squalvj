a3baa070340ab3808c03e24b96f848d6e7b74ecc
squalvj <squalvj@gmail.com>
2018-01-26 12:26:02 +0700

    init

---
+++ GruntFile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+var opt = ['assets/js/*.js', 'assets/css/main.css'];
+  // Project configuration.
+   grunt.initConfig({
+    injector: {
+      options: {
+        min:'true'
+      },
+      local_dependencies: {
+        files: {
+          'index.html': opt,
+        }
+      }
+    }
+  })
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-injector');
+
+  // Default task(s).
+  grunt.registerTask('default', ['injector']);
+
+};
